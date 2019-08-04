1226e7565a069b8872c2d21371e81d2297ceb1f2
kongou-ae <y.matsumoto.ae@gmail.com>
2019-08-05 00:46:24 +0900

    update

--- build/buildhtml.js
+++ build/buildhtml.js
@@ -23,6 +23,7 @@ const main = async ()=>{
         console.log("dist/guidline.pdf is not found ")
     }
 
+    // すべての Markdown を結合する
     let mdBook = await fs.readFileSync("README.md", {encoding: "utf-8"});
     mdBook += await fs.readFileSync("002_ハイブリッドクラウド概論.md", {encoding: "utf-8"});
     mdBook += await fs.readFileSync("003_スタイルマップ.md", {encoding: "utf-8"});
@@ -33,12 +34,21 @@ const main = async ()=>{
     mdBook += await fs.readFileSync("008_ハイブリッドクラウド研究会の活動への参加の誘い.md", {encoding: "utf-8"});
     mdBook += await fs.readFileSync("009_あとがき的なもの.md", {encoding: "utf-8"});
 
-    let htmlBook = md.render(mdBook);
+    // Media フォルダを build フォルダに移動
 
+    let files = fs.readdirSync('media');
+    files.forEach(element => {
+        console.log(element)
+        fs.copyFileSync("media/" + element, "build/media/" + element)
+    });
+
+    // Markdown を HTML に変換
+    let htmlBook = md.render(mdBook);
     htmlBook = htmlBook.replace(/<a href="[0-9]{3}_.*?#/g,'<a href="#')
     htmlBook = await fs.readFileSync("build/header.html", {encoding: "utf-8"}) + htmlBook;
     htmlBook += await fs.readFileSync("build/footer.html", {encoding: "utf-8"});
 
+    // PDF を書き出し
     await fs.writeFileSync("build/book.html",htmlBook,{ flag : 'w' })
     console.log("Generated build/book.html")
 }
